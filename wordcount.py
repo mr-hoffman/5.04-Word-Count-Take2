b87f3e221a7048baba9e4b70ba066107ea37a3dc
mr-hoffman <hoffmant@hilbertk12.org>
2025-03-07 10:56:07 -0600

    Create wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,35 @@
+###########################################################
+###   Copy and paste your word frequency code here      ###
+###   into two new functions:                           ###
+###      text_to_word_list()                            ###
+###      count_frequencies()                            ###
+###   OR add your word frequency code to thie repo as   ###
+###   a separate file and import it                     ###
+###########################################################
+
+# remove these if you are going to import your frequency module
+def text_to_word_list():
+  pass
+
+def count_frequencies():
+  pass
+
+
+###########################################################
+###   Write your new functions here:                    ###
+###      find_max_valued_key()                          ###
+###########################################################
+
+def find_max_valued_key():
+  pass
+
+
+###########################################################
+###   Write your loop to find the top                   ###
+###   five most frequent words                          ###
+###########################################################
+
+
+
+
+
